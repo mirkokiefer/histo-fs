c80814f4bc482268288837e0d6f8d9d3e541f5dc
Mirko Kiefer <mail@mirkokiefer.com>
2013-06-21 15:28:45 +0200

    example

--- test/index.js
+++ test/index.js
@@ -1,69 +1,80 @@
 
+// '/users/1' is the URN - the URL corresponds to a specific state of the resource.
+// The URN points to the position in the hierarchy with a unique ID as the last
+// path component.
+var jim = dictionary('/users/1', {
+  name: 'Jim'
+})
+var ann = dictionary('/users/2', {
+  name: 'Ann'
+})
 
-var store1 = new Store(Set)
+var prioritizeBacklog = dictionary('/projects/3/tasks/4', {
+  title: 'Prioritize backlog',
+  assignee: urn(jim)
+})
+var reviewIssues = dictionary('/projects/3/tasks/5', {
+  title: 'Review issues',
+  assignee: urn(ann)
+})
+var releasePlanningMembers = set('/projects/3/members', [urn(jim), urn(ann)])
+var releasePlanningTasks = orderedDictionary('/projects/3/tasks', {
+  '4': url(prioritizeBacklog),
+  '5': url(reviewIssues)
+})
 
-var commit1 = store1.reset([1, 2, 3])
+var releasePlanning = dictionary('/projects/3', {
+  name: 'Release Planning',
+  members: url(releasePlanningMembers),
+  tasks: url(releasePlanningTasks)
+})
 
-var commit2 = store1.write(commit1, {diff: {insert: [4, 5]}})
+var organizationProjects = dictionary('/projects', {
+  '3': url(releasePlanning)}
+})
+var organizationMembers = dictionary('/members', {
+  '1': url(jim)},
+  '2': url(ann)}
+})
 
-var commit3 = store1.write(commit1, {diff: {delete: [0]}})
+var organization = dictionary('/', {
+  members: url(organizationMembers),
+  projects: url(organizationProjects)
+})
 
-// now we have two branches (branches are just commits with no descendants)
-var branches = store1.branches()
+var commit = {
+  ancestors: [url(ancestorCommit)]
+  data: url(organization)
+}
 
-// merge them
-var mergedDiff = store1.merge(branches)
+/*
+The URN corresponds to the hierarchy where a structure is embedded.
+When updating a structure we have to pass the full path to it - so we know
+immediately which parent structures need to be updated as well.
+The alternative is to always persist the entire hierarchy.
 
-mergedDiff.resolveConflicts()
+We need some sort of staging area like git.
+So we can make multiple changes without committing every time.
 
-var commit4 = store1.write(branches, mergedDiff)
+The URLs are the revisions of a structure - kind of like couchdb documents...
+The difference to CouchDB is that we can at any time snapshot the state of our data.
+Whenever we have an update conflict at a dictionary entry with a hash as its value,
+we do a 3-way-merge.
+We have to be careful with picking IDs/keys. They should be based on the first version
+of a document's hash. This guarantees that no two concurrently created docs pick
+the same ID. At the same time it makes sure that if two users create the same
+doc concurrently it has the same ID.
 
-// reset just calculates the diff for you
-var commit5 = store1.reset(commit4, [3, 4, 5])
+Using CouchDB's terminology, each structure corresponds to a document.
+Each document is of a data type which has diff and merge semantics associated.
+Every change of a document creates a new revision.
+Dictionary documents may refer to other document revisions.
+If dictionary entries are conflicting we try to resolve them by merging
+the referenced documents.
+Conflicts on dictionary entries with literal values can obviously not be
+resolved this way.
 
-// I would simplify this to only allow one branch per store - so you dont need to track commits when writing
-// if you need multiple branches you just use multiple stores
-
-// events:
-
-store1.on('written', function(commit) {...})
-store1.on('write', function(oldCommit, newCommit) {...})
-// commits are just hash of the parent commit + diff hash
-
-/* ok but how do the syncing??
-
-ask server for diff between last sync (common ancestor) and his head
-do a local 3-way merge
-push diff to server and update head
-
-ok what if we don't know our common ancestor with the server?
-then we can exchange a full list of commits OR just transfer the full data
-
-thats cool because it means all nodes can decide to forget history and syncing still works fine - just less efficient.
-
-ok this doesnt work in a distributed way
-we have to transfer all interim commits and their data as well!
-
-ideal sync process is:
-push all commits since last synced commit
-ask server for common ancestor of local head and remote head
-push all local data since common ancestor
-if common ancestor = remote head
-  try fast-forward remote head
-  if failed (someone else updated head in the meantime) then start over
-else
-  ask for all remote commits + data since common ancestor
-  do local merge
-  start over
+I'd like to have extensible design where developers can add support for new
+doc types.
 */
 
-var remote = new RemoteStore(Set, url)
-remote.head(function(err, head) {
-  remote.diff(lastSyncedCommit, remoteHead, function(err, remoteDiff) {
-    var mergedDiff = merge([localDiff, remoteDiff])
-    // resolve conflicts!
-    store1.write([localHead, remoteHead], lastSyncedCommit, mergedDiff)
-    remote.write([localHead, remoteHead], lastSyncedCommit, mergedDiff)
-  })
-})
-var diff = remote.diff
