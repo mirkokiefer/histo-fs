2096ea94fbe882144a4d79c3f0475c719c1f1654
Mirko Kiefer <mail@mirkokiefer.com>
2013-06-24 18:00:10 +0200

    don't use stage head for id computation on post

--- test/index.js
+++ test/index.js
@@ -35,8 +35,8 @@ describe('read/write to database', function() {
   it('should post jim as child resource', function(done) {
     db.post('/members', jim, function(err, res) {
       assert.deepEqual(res, {
-        path: '/members/86e493a252bcbbf0364967152722d3f9aeac0fb9',
-        hash: '86cd722c67a3ea77c5471a516bbd6e0dc410c5c3'
+        "path":"/members/ddd0a27f2f483ef3117adb93b0153f5beb3e148c",
+        "hash":"86cd722c67a3ea77c5471a516bbd6e0dc410c5c3"
       })
       done()
     })
@@ -44,15 +44,15 @@ describe('read/write to database', function() {
   it('should post ann as child resource', function(done) {
     db.post('/members', ann, function(err, res) {
       assert.deepEqual(res, {
-        path: '/members/49a6707e81de4284c5baea0b3ac2cf890ef74ec2',
-        hash: '5998e29c9003e1d2d2be515d0bdb571d191e4210'
+        "path":"/members/9c37ba065ec42fe4f900b7452b81888ffc04615a",
+        "hash":"5998e29c9003e1d2d2be515d0bdb571d191e4210"
       })
       done()
     })
   })
   it('should retrieve the updated root resource', function(done) {
     var expected = {"dictionary":{
-      "members":"d05e54c8f39d2d8a9a1ddacac1c66d0514f7022e",
+      "members":"02482fd84dd3ef7d23915bcf34e8bba3d271782e",
       "name":"LivelyCode"
     }}
     db.get('/', function(err, res) {
@@ -61,9 +61,9 @@ describe('read/write to database', function() {
     })
   })
   it('should retrieve the list of members', function(done) {
-    var expected = {dictionary: {
-      '49a6707e81de4284c5baea0b3ac2cf890ef74ec2': '5998e29c9003e1d2d2be515d0bdb571d191e4210',
-      '86e493a252bcbbf0364967152722d3f9aeac0fb9': '86cd722c67a3ea77c5471a516bbd6e0dc410c5c3'
+    var expected = { dictionary: {
+      '9c37ba065ec42fe4f900b7452b81888ffc04615a': '5998e29c9003e1d2d2be515d0bdb571d191e4210',
+      'ddd0a27f2f483ef3117adb93b0153f5beb3e148c': '86cd722c67a3ea77c5471a516bbd6e0dc410c5c3'
     } }
     db.get('/members', function(err, res) {
       assert.deepEqual(res, expected)
@@ -71,13 +71,13 @@ describe('read/write to database', function() {
     })
   })
   it('should retrieve jim', function(done) {
-    db.get('/members/86e493a252bcbbf0364967152722d3f9aeac0fb9', function(err, res) {
+    db.get('/members/ddd0a27f2f483ef3117adb93b0153f5beb3e148c', function(err, res) {
       assert.deepEqual(res, jim)
       done()
     })
   })
   it('should retrieve ann', function(done) {
-    db.get('/members/49a6707e81de4284c5baea0b3ac2cf890ef74ec2', function(err, res) {
+    db.get('/members/9c37ba065ec42fe4f900b7452b81888ffc04615a', function(err, res) {
       assert.deepEqual(res, ann)
       done()
     })
