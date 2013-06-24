ff83bbd824b930b3f16ff33664eeb8d2248f40bc
Mirko Kiefer <mail@mirkokiefer.com>
2013-06-24 13:24:04 +0200

    move pseudo test to example

--- example.js
+++ example.js
@@ -78,3 +78,43 @@ I'd like to have extensible design where developers can add support for new
 doc types.
 */
 
+// some more concrete examples:
+
+var organization = new Dictionary({
+  members: new Dictionary(),
+  projects: new Dictionary()
+})
+
+db.put('/', Dictionary, organization)
+
+var jim = new Dictionary({
+  name: 'jim'
+})
+
+db.post('/members', jim)
+
+var project = new Dictionary({
+  name: 'My Project',
+  members: new Set(),
+  tasks_order: new OrderedSet(),
+  tasks: new Dictionary()
+})
+
+db.post('/projects', project)
+
+var prioritizeBacklog = new Dictionary({
+  title: 'Prioritize backlog',
+  assignee: jim.uri()
+})
+
+db.post(project.uri() + '/tasks', prioritizeBacklog)
+
+db.put(project.uri() + '/tasks_order', [prioritizeBacklog.id()])
+
+db.put(project.uri(), project, function(err, res) {
+  // conflict because project has been changed by adding a task
+  assert.ok(res.conflict)
+})
+
+
+
